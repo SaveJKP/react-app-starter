cdf8f95c9646b83554bb9d5b82bfd9cf133be398
Jakapat Pattanasiri <jakapat_pattanasiri@hotmail.com>
2025-03-28 11:19:27 +0700

    feat(file): apgrade tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 export default function App() {
   return (
-    <div className="h-screen flex justify-center bg-blue-950">
-      <div className="p-6 gap-y-6 flex flex-col justify-start w-[80%] lg:w-[70%]">
-        <h1 className="w-full p-6 bg-amber-100 font-extrabold">
+    <div className="flex justify-center bg-blue-950">
+      <div className="flex w-[80%] flex-col justify-start gap-y-6 p-6 lg:w-[70%]">
+        <h1 className="w-full bg-amber-100 p-6 font-extrabold">
           React App Starter
         </h1>
-        <section className="w-full p-5 bg-amber-100 flex">
-          <ul className="list-inside list-disc flex-1">
+        <section className="flex w-full bg-amber-100 p-5">
+          <ul className="flex-1 list-inside list-disc">
             <span className="font-semibold">Tech Stack:</span>
             <li>Vite</li>
             <li>React</li>
